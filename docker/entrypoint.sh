678e21a4d657f99603783da845a56d6bfdf5e14b
Achilleas Seisa <achilleas2942@gmail.com>
2024-10-23 00:11:00 +0200

    Add Docker and Kubernetes files

---
+++ docker/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Source the ROS setup
+source /opt/ros/noetic/setup.bash
+
+# Pull the latest updates from the repositories
+cd /root/catkin_ws/src
+if [ -d "cnmpc_recording" ]; then
+    cd cnmpc_recording && git pull && cd ..
+else
+    git clone git@github.com:achilleas2942/cnmpc_recording.git
+fi
+
+# Build the workspace
+cd /root/catkin_ws/
+catkin_make
+
+# Source the workspace
+source /root/catkin_ws/devel/setup.bash
+
+# Execute the provided command
+exec "$@"
---
+++ src/run_k8s_resource_metrics.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /root/catkin_ws/devel/setup.bash
+python3 /root/catkin_ws/src/cnmpc_recording/src/k8s_resource_metrics.py
